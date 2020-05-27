3be44c85db20f5b130767aa132c19f46dde299a9
TomMarazzo <camenm@hotmail.com>
2020-05-27 16:32:48 -0400

    Created a mock model of a product until I get the Dbase set up next time!

--- Controllers/StoreController.cs
+++ Controllers/StoreController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GeorgianComputers.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeorgianComputers.Controllers
@@ -10,7 +11,15 @@ namespace GeorgianComputers.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            //Make a list of mock products
+            var products = new List<Product>();
+            //use a for loop to populate the product list
+            for(var i = 1; i <= 10; i++)
+            {
+                products.Add(new Product { Name = "Product " + i.ToString() });
+            }
+            //pass the product list to the view so it can be displayed to user.
+            return View(products);
         }
         public IActionResult Details(string product)
         {
