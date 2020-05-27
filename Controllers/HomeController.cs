1eff3c4b028053d58389ca74510cdd94068a3d43
TomMarazzo <camenm@hotmail.com>
2020-05-27 11:55:17 -0400

    I've added the About View to the project and connected to the Nav Bar.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace GeorgianComputers.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
