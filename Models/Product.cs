d6425f56be1eed2d28162afa877fe7dc9003285d
TomMarazzo <camenm@hotmail.com>
2020-06-10 13:00:18 -0400

    Updated some currency formating and validatio edits

--- Models/Product.cs
+++ Models/Product.cs
@@ -21,6 +21,8 @@ namespace GeorgianComputers.Models
         [StringLength(8000)]
         public string Description { get; set; }
         [Column(TypeName = "decimal(10, 2)")]
+        [Range(0, 999999, ErrorMessage = "Price must be between 0 and 999,999")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
         public decimal Price { get; set; }
         [StringLength(255)]
         public string Photo { get; set; }
